f55535c65955d9ac1f504ecbf98bd9be937df077
Jose Jaime Figueroa Barrios <jaimefigueroa84@gmail.com>
2019-01-14 08:55:43 -0700

    Componentes Puros y Funcionales en React

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Media from "./media.js";
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist;
-    console.log(this.props.data);
-    return (
-      <div>
-        {playlist.map(item => {
-          return <Media {...item} key={item.id} />;
-        })}
-      </div>
-    );
-  }
+function Playlist(props) {
+  const playlist = props.data.categories[0].playlist;
+  console.log(props.data);
+  return (
+    <div>
+      {playlist.map(item => {
+        return <Media {...item} key={item.id} />;
+      })}
+    </div>
+  );
 }
 
 export default Playlist;
