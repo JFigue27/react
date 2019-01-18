f9b3c2e6e1f550cf811ae891fe442224bd47cc90
Jose Jaime Figueroa Barrios <jaimefigueroa84@gmail.com>
2019-01-18 14:36:48 -0700

    Play y Pausa

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import VideoPlayerLayout from "../components/video-player-layout";
 import Video from "../components/video";
 import Title from "../components/title";
+import PlayPause from "../components/play-pause";
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true
+  };
+  togglePlay = event => {
+    this.setState({
+      pause: !this.state.pause
+    });
+  };
   render() {
     return (
       <VideoPlayerLayout>
         <Title title="Esto es un video chido!" />
-        <Video autoplay={false} src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
+        <PlayPause pause={this.state.pause} handleClick={this.togglePlay} />
+        <Video
+          autoplay={this.props.autoplay}
+          pause={this.state.pause}
+          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        />
       </VideoPlayerLayout>
     );
   }
