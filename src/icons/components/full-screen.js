59627df7d5dcb7fbb135924d5003782fb9dd8a0d
Jose Jaime Figueroa Barrios <jaimefigueroa84@gmail.com>
2019-01-19 21:57:10 -0700

    Fix Props for Icons

--- src/icons/components/full-screen.js
+++ src/icons/components/full-screen.js
@@ -3,7 +3,7 @@ import Icon from "./icon";
 
 function FullScreen(props) {
   return (
-    <Icon>
+    <Icon {...props}>
       <path d="M32 0h-13l5 5-6 6 3 3 6-6 5 5z" />
       <path d="M32 32v-13l-5 5-6-6-3 3 6 6-5 5z" />
       <path d="M0 32h13l-5-5 6-6-3-3-6 6-5-5z" />
